fadce0e290650f5ea539dbb953dd80a2f3efb147
冰舞网络 <357145480@qq.com>
2021-03-18 13:46:32 +0800

    ''

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>B5AdminCMF</title>
+    <title>B5LaravelCMF</title>
     <!--[if lt IE 9]>
     <meta http-equiv="refresh" content="0;url=/ie.html"/>
     <![endif]-->
